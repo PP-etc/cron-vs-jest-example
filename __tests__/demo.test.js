639d596365e85027e24acf2e672cc7e49ad1d0d9
Pavel Polyakov <me@pavelpolyakov.com>
2020-05-11 09:55:58 +0200

    + modern example

--- __tests__/demo.test.js
+++ __tests__/demo.test.js
@@ -23,6 +23,22 @@ test("advanceTimersByTime works", (done) => {
   done();
 });
 
+test("advanceTimersByTime works (modern)", (done) => {
+  jest.useFakeTimers("modern");
+  const job = new CronJob({
+    cronTime: "* * * * *",
+    onTick: function () {
+      callback();
+    },
+    start: true,
+  });
+  jest.advanceTimersByTime(2 * 60 * 1000);
+
+  expect(callback).toBeCalledTimes(2);
+  job.stop();
+  done();
+});
+
 test("sinon clock.tick works", (done) => {
   const clock = sinon.useFakeTimers();
   const job = new CronJob({
